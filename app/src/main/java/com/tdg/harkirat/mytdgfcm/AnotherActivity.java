ca9301219470fa2f3c1179f897f7a325d846a96a
Kirat3777 <harkiratsingh3777@gmail.com>
2018-05-09 15:47:52 +0530

    first commit

---
+++ app/src/main/java/com/tdg/harkirat/mytdgfcm/AnotherActivity.java
@@ -0,0 +1,37 @@
+package com.tdg.harkirat.mytdgfcm;
+
+/**
+ * Created by Dell on 5/5/2018.
+ */
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+
+/**
+ * Created by AndroidBash on 20-Aug-16.
+ */
+public class AnotherActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_another);
+
+        if (getIntent().getExtras() != null) {
+
+            for (String key : getIntent().getExtras().keySet()) {
+                String value = getIntent().getExtras().getString(key);
+
+                if (key.equals("AnotherActivity") && value.equals("True")) {
+                    Intent intent = new Intent(this, AnotherActivity.class);
+                    intent.putExtra("value", value);
+                    startActivity(intent);
+                    finish();
+                }
+
+            }
+        }
+    }
+}
